71a1f1c264e6eddb1107f21831be9766d00e3fd3
NO GAME NO LIFE <qqlcx5@163.com>
2019-04-11 23:53:26 +0800

    first commit

--- socket.io_1/push.sh
+++ socket.io_1/push.sh
@@ -2,5 +2,4 @@ cd ..
 git add .
 git commit -m "first commit"
 git remote add origin git@github.com:qqlcx5/demo_socket.io.git
-git pull -u origin master
-git push -u origin master
+git push -u -f origin master
